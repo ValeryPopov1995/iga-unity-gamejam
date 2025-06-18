f964316c88a08a87c39aa37ad60019b13e98bcdf
Valery <valerypopov1995@gmail.com>
2025-06-18 21:21:29 +0300

    feat: animations

--- Assets/AncestralPotatoes/Mechanics/Character/Scripts/PlayerAnimator.cs
+++ Assets/AncestralPotatoes/Mechanics/Character/Scripts/PlayerAnimator.cs
@@ -1,15 +1,41 @@
-﻿using NaughtyAttributes;
+﻿using Cysharp.Threading.Tasks;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace AncestralPotatoes.Character
 {
     public class PlayerAnimator : MonoBehaviour
     {
-        [SerializeField] private Animator animator;
-        [SerializeField, AnimatorParam(nameof(animator))] private string strike, fire, move;
-
         public void Strike() => animator.SetTrigger(strike);
         public void Fire() => animator.SetTrigger(fire);
         public void Move(bool move) => animator.SetBool(this.move, move);
+
+        [SerializeField] private Animator animator;
+        [SerializeField] private InputAction moveAction;
+        [SerializeField, AnimatorParam(nameof(animator))] private string strike, fire, move;
+        [SerializeField] private int updateDelay = 500;
+        private bool moved;
+
+        private async void OnEnable()
+        {
+            moveAction.Enable();
+
+            while (this != null)
+            {
+                var moved = moveAction.ReadValue<Vector2>().magnitude > 0;
+                if (this.moved != moved)
+                {
+                    this.moved = moved;
+                    Move(this.moved);
+                }
+                await UniTask.Delay(updateDelay);
+            }
+        }
+
+        private void OnDisable()
+        {
+            moveAction.Disable();
+        }
     }
 }
